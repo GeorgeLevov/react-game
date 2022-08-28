7b318003287ccd1bb86ca49fabc8fd5c325402da
George Levov <georgelevov@gmail.com>
2022-08-28 16:54:03 +0300

    Created Card components and reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import ClubImage from "./assets/icons8-clubs-64.png";
 import DiamondImage from "./assets/icons8-diamond-64.png";
 import HeartImage from "./assets/icons8-heart-64.png";
 import SpadeImage from "./assets/icons8-spades-64.png";
+import { nanoid } from "nanoid";
 
 function App() {
-    const imageArray = [ClubImage, DiamondImage, HeartImage, SpadeImage];
+    // array of the images that we're going to need for the game
+    const cardFaces = [ClubImage, DiamondImage, HeartImage, SpadeImage];
+
+    // generating the individual card components inside the function below,
+    // maybe the function can be broken down to a function creating card objects whose keys are then piped into components
+    // but the Card component it's simple enough that it doesn't need to be reworked
+    const generateCards = (count) => {
+        const cardArray = Array.apply(null, Array(count)).map(() => {
+            return (
+                <Card
+                    key={nanoid()}
+                    lockedIn={false}
+                    value={
+                        cardFaces[Math.floor(Math.random() * cardFaces.length)]
+                    }
+                ></Card>
+            );
+        });
+        return cardArray;
+    };
+
+    const [cards, setCards] = useState(generateCards(8));
+
+    const resetCards = () => {
+        setCards(() => generateCards(8));
+    };
 
     return (
         <main>
-            <div className="card-container">
-                <Card
-                    key={1}
-                    value={
-                        imageArray[
-                            Math.floor(Math.random() * imageArray.length)
-                        ]
-                    }
-                ></Card>
-            </div>
+            <div className="card-container">{cards}</div>
+
+            <button className="card-reset-btn" onClick={resetCards}>
+                Set Cards
+            </button>
         </main>
     );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import "./Card.css";
 
-export default function Card({ value }) {
-    const [count, setCount] = useState(0);
+export default function Card({ key, value, lockedIn }) {
+    // const [count, setCount] = useState(0);
 
     return (
-        <div className="card">
+        <div id={key} className="card">
             <img src={value}></img>
         </div>
     );
